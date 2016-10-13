a5064fca0a8f3b9977662b1a9e390408cccd21a6
Isaac Allen <nellacaasi@gmail.com>
2016-10-13 12:29:04 +0100

    Basic Structure Setup

---
+++ src/controllers/BruteAutoComplete.java
@@ -0,0 +1,23 @@
+package controllers;
+
+public class BruteAutoComplete implements AutoComplete {
+
+	@Override
+	public double weightOf(String term) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String bestMatch(String prefix) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<String> matches(String prefix, int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
